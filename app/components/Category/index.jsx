6c0144392ab87364cbd5dc070b3f4c3c44e1931a
fergus <fergus.jingtai@gmail.com>
2017-11-06 02:24:40 +0800

    7-10 获取轮播图 index

--- app/components/Category/index.jsx
+++ app/components/Category/index.jsx
@@ -4,9 +4,18 @@ import ReactSwipe from 'react-swipe';
 import './style.less';
 
 class Category extends Component {
+  state = {
+    index: 0
+  }
   render() {
+    const opt = {
+      auto: 2000,
+      callback: (index) => {
+        this.setState({ index });
+      }
+    };
     return (
-      <ReactSwipe className="carousel" swipeOptions={{continuous: false}}>
+      <ReactSwipe className="carousel" swipeOptions={opt}>
         <div>PANE 1</div>
         <div>PANE 2</div>
         <div>PANE 3</div>
