b6e345b87be73fe4b556bc97c599b6bbdd00707b
Fergus <fergus.jingtai@gmail.com>
2017-11-06 16:03:40 +0800

    7-16 猜你喜欢-获取列表数据

--- app/containers/Home/subpage/List.jsx
+++ app/containers/Home/subpage/List.jsx
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
+import { getListData } from '../../../fetch/home/home.js';
+
+import './style.less';
 
 class List extends Component {
-  render() {
+  state = {
+    data: [],
+    hasMore: false
+  }
+
+  componentDidMount() {
+    this.loadFirstPageData();
+  }
+
+  // 获取首页数据
+  loadFirstPageData = () => {
     const { cityName } = this.props;
+    const result = getListData(cityName, 0);
+    this.resultHandle(result);
+  }
+
+  resultHandle = (result) => {
+    result
+      .then(res => res.json())
+      .then((json) => {
+        const { data, hasMore } = json;
+
+        this.setState({
+          data,
+          hasMore
+        });
+      });
+  }
+
+  render() {
+    // const { cityName } = this.props;
 
     return (
       <div>
-        List {cityName}
+        <h2 className="home-list-title">猜你喜欢</h2>
       </div>
     );
   }
