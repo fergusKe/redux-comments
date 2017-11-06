325f7d325ba4a76d104bf6828baf1661e15bdff9
Fergus <fergus.jingtai@gmail.com>
2017-11-06 16:43:24 +0800

    7-18 猜你喜欢-显示内容

--- app/components/List/item/index.jsx
+++ app/components/List/item/index.jsx
@@ -2,13 +2,30 @@ import React, { Component } from 'react';
 
 import './style.less';
 
+const img = require('../../../static/img/3.png');
+
 class Item extends Component {
   render() {
     const { data } = this.props;
 
     return (
-      <div>
-        {data.title}
+      <div className="list-item clear-fix">
+        <div className="item-img-container float-left">
+          <img src={img} alt={data.title} />
+        </div>
+        <div className="item-content">
+          <div className="item-title-container clear-fix">
+            <h3 className="float-left">{data.title}</h3>
+            <span className="float-right">{data.distance}</span>
+          </div>
+          <p className="item-sub-title">
+            {data.subTitle}
+          </p>
+          <div className="item-price-container clear-fix">
+            <span className="price float-left">￥{data.price}</span>
+            <span className="mumber float-right">已售{data.mumber}</span>
+          </div>
+        </div>
       </div>
     );
   }
