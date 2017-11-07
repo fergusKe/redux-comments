8080207085bb51bec2063375879d7b90d7757184
Fergus <fergus.jingtai@gmail.com>
2017-11-07 17:09:03 +0800

    8-5 修改城市的方法

---
+++ app/components/CityList/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './style.less';
+
+class CityList extends Component {
+  render() {
+    return (
+      <div>
+        CityList
+      </div>
+    );
+  }
+}
+
+export default CityList;
--- app/containers/City/index.jsx
+++ app/containers/City/index.jsx
@@ -5,10 +5,28 @@ import { withRouter } from 'react-router-dom';
 
 import Header from '../../components/Header';
 import CurrentCity from '../../components/CurrentCity';
+import CityList from '../../components/CityList';
 
+import LocalStore from '../../util/localStore.js';
+import { CITYNAME } from '../../config/localStoreKey.js';
 import * as userInfoActionsFromOtherFile from '../../actions/userinfo.js';
 
 class City extends Component {
+  changeCity = (newCity) => {
+    if (newCity == null) return;
+
+    // 修改 redux
+    const { userinfo, userInfoActions } = this.props;
+    userinfo.cityName = newCity;
+    userInfoActions.update(userinfo);
+
+    // 修改 cookie
+    LocalStore.setItem(CITYNAME, newCity);
+
+    // 跳轉到首頁
+
+  }
+
   render() {
     const { userinfo } = this.props;
 
@@ -16,6 +34,7 @@ class City extends Component {
       <div>
         <Header title="選擇城市" />
         <CurrentCity cityName={userinfo.cityName} />
+        <CityList changeFn={this.changeCity} />
       </div>
     );
   }
