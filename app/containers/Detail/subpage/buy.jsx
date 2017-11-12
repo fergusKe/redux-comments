ab48a5b677b838f98211cca1097a8df90c938ccc
fergus <fergus.jingtai@gmail.com>
2017-11-13 00:41:55 +0800

    12-2 购买功能

--- app/containers/Detail/subpage/buy.jsx
+++ app/containers/Detail/subpage/buy.jsx
@@ -8,12 +8,31 @@ class Buy extends Component {
 
   // 购买事件
   buyHandle = () => {
+    // 验证登录，未登录则retur
+    const loginFlag = this.storeHandle();
+    if (!loginFlag) return;
 
+    // 此过程为模拟购买，因此可省去复杂的购买过程
+
+    // 跳转到用户主页
+    this.props.history.push('/user');
   }
 
   // 收藏事件
   storeHandle = () => {
 
+  }
+
+  // 检查登录状态
+  loginCheck = () => {
+    const { id, userinfo } = this.props;
+
+    if (!userinfo.username) {
+      // 跳转到登录页面的时候，要传入目标router，以便登录完了可以自己跳转回来
+      this.props.history.push(`/login/${encodeURIComponent(`/detail/${id}`)}`);
+      return false;
+    }
+    return true;
   }
 
   render() {
