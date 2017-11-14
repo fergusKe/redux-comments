47982ff16a6b29ae78741eb3fc66f054cdca0456
fergus <fergus.jingtai@gmail.com>
2017-11-14 08:36:35 +0800

    13-1 Header

--- app/containers/User/index.jsx
+++ app/containers/User/index.jsx
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+import Header from '../../components/Header';
 
 class User extends Component {
+  componentDidMount() {
+    // 如果未登录，跳转到登录页面
+    if (!this.props.userinfo.username) {
+      this.props.history.push('/login');
+    }
+  }
+
   render() {
     return (
       <div>
-        User
+        <Header title="用户主页" backRouter="/" history={this.props.history} />
       </div>
     );
   }
 }
 
-export default User;
+const mapStateToProps = state => ({
+  userinfo: state.userinfo
+});
+
+export default withRouter(connect(mapStateToProps)(User));
